accd1459d10df1e29aa6b543ef49c965471aa207
yasinmnk <77920164+yasinmnk@users.noreply.github.com>
2021-02-17 20:59:49 +0200

    Create async-await.js

---
+++ async-await.js
@@ -0,0 +1,55 @@
+const cart = {
+    isim: "Yasin",
+    bakiye: 50,
+}
+
+function biletOku() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(console.log("Kart Okunuyor.."))
+
+        })
+    }, 2000)
+}
+
+function bilgiGoster(cart) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (cart) {
+                console.log(`Bakiyeniz: ${cart.bakiye}`)
+                resolve()
+            } else {
+                console.log("kart bulunamadi")
+                reject()
+            }
+
+
+
+        }, 1000)
+    })
+}
+
+function paraCek(cart) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            if (cart) {
+                cart.bakiye -= 2
+                console.log(`Kalan Bakiyeniz: ${cart.bakiye}`)
+                resolve()
+            } else {
+                reject(console.log("kart bulunamadi"))
+            }
+        }, 500)
+    })
+
+
+}
+
+async function yasin() {
+    await biletOku()
+    await bilgiGoster(cart)
+    await paraCek(cart)
+
+}
+yasin()
